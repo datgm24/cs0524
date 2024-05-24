784e126e0a0a93b4fdac0929c0648ea0f9fd7387
YuTanaka <tanakaedu@users.noreply.github.com>
2024-05-24 16:10:03 +0900

    変数

--- cs0524/Form1.Designer.cs
+++ cs0524/Form1.Designer.cs
@@ -33,13 +33,17 @@
             label1 = new Label();
             textBox1 = new TextBox();
             timer1 = new System.Windows.Forms.Timer(components);
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(12, 12);
+            button1.Location = new Point(21, 24);
+            button1.Margin = new Padding(5, 6, 5, 6);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(129, 46);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
@@ -48,34 +52,73 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(1, 60);
+            label1.Location = new Point(241, 243);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(71, 30);
             label1.TabIndex = 1;
             label1.Text = "label1";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 96);
+            textBox1.Location = new Point(21, 192);
+            textBox1.Margin = new Padding(5, 6, 5, 6);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(169, 36);
             textBox1.TabIndex = 2;
             textBox1.Text = "たなか";
             // 
             // timer1
             // 
             timer1.Enabled = true;
-            timer1.Interval = 10;
+            timer1.Interval = 50;
             timer1.Tick += timer1_Tick;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(183, 24);
+            button2.Margin = new Padding(5, 6, 5, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 46);
+            button2.TabIndex = 3;
+            button2.Text = "演習7-1";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(322, 24);
+            button3.Margin = new Padding(5, 6, 5, 6);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 46);
+            button3.TabIndex = 4;
+            button3.Text = "演習7-2";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(459, 24);
+            button4.Name = "button4";
+            button4.Size = new Size(128, 46);
+            button4.TabIndex = 5;
+            button4.Text = "演習7-4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(384, 450);
+            ClientSize = new Size(658, 449);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
+            Font = new Font("Yu Gothic UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5, 6, 5, 6);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -88,5 +131,8 @@
         private Label label1;
         private TextBox textBox1;
         private System.Windows.Forms.Timer timer1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
